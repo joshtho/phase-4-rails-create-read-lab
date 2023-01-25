3ff5298e4d2e0f286bf54ee40a6a306e6325394d
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-01-25 15:12:19 -0600

    commit tree aef9c3ef92941b4841b5f19ae976ad55a9fc1b89

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -14,7 +14,13 @@ class PlantsController < ApplicationController
     end
 
     def create
-        plant = Plant.create(params.permit(:name, :image, :price))
+        plant = Plant.create(plant_params)
         render json: plant, status: :created
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
     end
 end
